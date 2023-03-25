24cb00581c3b74c244c52e0d39db2b4ac436f1bb
Hayden <115202535+haydenkress@users.noreply.github.com>
2023-03-25 15:16:48 -0400

    Add styling and remove duplicate entry issue

--- app.js
+++ app.js
@@ -5,13 +5,113 @@ const myLibrary = [];
 const noBooks = document.createElement("div");
 noBooks.classList.add("no-books");
 const noBookWarningSpot = document.querySelector(".no-book-warning-div");
-if (myLibrary.length >= 0) {
-  noBookWarning();
-}
+noBookWarning();
 
 // book table
 const bookList = document.getElementById("book-list");
 const form = document.querySelector("form");
+
+// constructor
+function Book(name, author, status) {
+  (this.name = name), (this.author = author), (this.status = status);
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); // prevent form from submitting
+
+  if (form.checkValidity()) {
+    createBook();
+  } else {
+    form.reportValidity();
+  }
+}
+
+function createBook() {
+  const title = document.getElementById("name").value;
+  const author = document.getElementById("author").value;
+  const status = document.getElementById("status").value;
+
+  const book = new Book(title, author, status);
+
+  myLibrary.push(book);
+  form.reset();
+  addBookToLibrary(book);
+  noBookWarning();
+}
+
+function addBookToLibrary(book) {
+  /*   myLibrary.forEach((book, index) => {
+    if (bookList.contains(book)) {
+      return; // Skip this book if a row already exists
+    }
+ */
+  const bookRow = document.createElement("tr");
+  bookList.appendChild(bookRow);
+
+  const titleCell = document.createElement("td");
+  titleCell.textContent = book.name;
+  bookRow.appendChild(titleCell);
+
+  // Add the book author to the row
+  const authorCell = document.createElement("td");
+  authorCell.textContent = book.author;
+  bookRow.appendChild(authorCell);
+
+  // Add the book read status to the row
+  const statusCell = document.createElement("td");
+  const statusSelect = document.createElement("select");
+
+  const notRead = document.createElement("option");
+  notRead.value = "not-read";
+  notRead.text = "Not Started";
+  const inProgress = document.createElement("option");
+  inProgress.value = "in-progress";
+  inProgress.text = "In Progress";
+  const completed = document.createElement("option");
+  completed.value = "completed";
+  completed.text = "Completed";
+  const didNotFinish = document.createElement("option");
+  didNotFinish.value = "did-not-finish";
+  didNotFinish.text = "DNF";
+  statusSelect.value = book.status;
+  statusSelect.appendChild(notRead);
+  statusSelect.appendChild(inProgress);
+  statusSelect.appendChild(completed);
+  statusSelect.appendChild(didNotFinish);
+  if (book.status === "not-read") {
+    notRead.selected = true;
+  } else if (book.status === "in-progress") {
+    inProgress.selected = true;
+  } else if (book.status === "completed") {
+    completed.selected = true;
+  } else if (book.status === "did-not-finish") {
+    didNotFinish.selected = true;
+  }
+
+  statusSelect.addEventListener("change", () => {
+    book.status = statusSelect.value;
+  });
+  statusCell.appendChild(statusSelect);
+  bookRow.appendChild(statusCell);
+
+  // Add a delete button to the row
+  const deleteCell = document.createElement("td");
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.classList.add("delete-button");
+  deleteButton.addEventListener("click", () => {
+    // Remove the book from the array
+    myLibrary.splice(book.index, 1);
+    // Remove the row from the table
+    bookRow.remove();
+    noBookWarning();
+  });
+  deleteCell.appendChild(deleteButton);
+  bookRow.appendChild(deleteCell);
+
+  // Add the new row to the book list
+  bookList.appendChild(bookRow);
+}
 
 function noBookWarning() {
   if (myLibrary.length === 0) {
@@ -23,73 +123,4 @@ function noBookWarning() {
       noBookWarningSpot.removeChild(noBooks);
     }
   }
-}
-
-function Book(name, author, status) {
-  (this.name = name), (this.author = author), (this.status = status);
-}
-
-function handleSubmit(event) {
-  event.preventDefault(); // prevent form from submitting
-
-  if (form.checkValidity()) {
-    createBook(event);
-  } else {
-    form.reportValidity();
-  }
-}
-
-function createBook() {
-  const name = document.getElementById("name").value;
-  const author = document.getElementById("author").value;
-  const status = document.getElementById("status").value;
-
-  const book = new Book(name, author, status);
-
-  myLibrary.push(book);
-
-  document.getElementById("name").value = "";
-  document.getElementById("author").value = "";
-  document.getElementById("status").selectedIndex = 0;
-
-  addBookToLibrary(myLibrary);
-  noBookWarning();
-}
-
-function addBookToLibrary(myLibrary) {
-  myLibrary.forEach((book, index) => {
-    const bookRow = document.createElement("tr");
-    bookList.appendChild(bookRow);
-
-    const titleCell = document.createElement("td");
-    titleCell.textContent = book.title;
-    bookRow.appendChild(titleCell);
-
-    // Add the book author to the row
-    const authorCell = document.createElement("td");
-    authorCell.textContent = book.author;
-    bookRow.appendChild(authorCell);
-
-    // Add the book read status to the row
-    const readCell = document.createElement("td");
-    readCell.textContent = book.read ? "Yes" : "No";
-    bookRow.appendChild(readCell);
-
-    // Add a delete button to the row
-    const deleteCell = document.createElement("td");
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.addEventListener("click", () => {
-      // Remove the book from the array
-      myLibrary.splice(index, 1);
-      // Remove the row from the table
-      bookRow.remove();
-      noBookWarning();
-    });
-    deleteCell.appendChild(deleteButton);
-    bookRow.appendChild(deleteCell);
-
-    // Add the new row to the book list
-    bookList.appendChild(bookRow);
-  });
 }
