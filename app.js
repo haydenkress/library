001fe014d549cfa2e18f34817f2216b64e06efcd
Hayden <115202535+haydenkress@users.noreply.github.com>
2023-03-25 12:54:40 -0400

    Fix book row generator and delete row function

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-let myLibrary = [];
-const label = document.createElement("div");
+// main array
+const myLibrary = [];
+
+// no book warning
+const noBooks = document.createElement("div");
+noBooks.classList.add("no-books");
+const noBookWarningSpot = document.querySelector(".no-book-warning-div");
+if (myLibrary.length >= 0) {
+  noBookWarning();
+}
+
+// book table
 const bookList = document.getElementById("book-list");
-const topRow = document.querySelector(".top-row");
-const submitButton = document.querySelector("#submit-button");
-submitButton.addEventListener("click", addBookToLibrary);
+const form = document.querySelector("form");
+
+function noBookWarning() {
+  if (myLibrary.length === 0) {
+    noBooks.textContent =
+      "You haven't added any books yet. Add books by entering the details above.";
+    noBookWarningSpot.appendChild(noBooks);
+  } else {
+    if (noBookWarningSpot.contains(noBooks)) {
+      noBookWarningSpot.removeChild(noBooks);
+    }
+  }
+}
 
 function Book(name, author, status) {
   (this.name = name), (this.author = author), (this.status = status);
 }
 
-function addBookToLibrary(event) {
-  event.preventDefault();
+function handleSubmit(event) {
+  event.preventDefault(); // prevent form from submitting
 
+  if (form.checkValidity()) {
+    createBook(event);
+  } else {
+    form.reportValidity();
+  }
+}
+
+function createBook() {
   const name = document.getElementById("name").value;
   const author = document.getElementById("author").value;
   const status = document.getElementById("status").value;
@@ -19,49 +47,49 @@ function addBookToLibrary(event) {
   const book = new Book(name, author, status);
 
   myLibrary.push(book);
+
   document.getElementById("name").value = "";
   document.getElementById("author").value = "";
   document.getElementById("status").selectedIndex = 0;
 
-  if (myLibrary.length === 1) {
-    bookList.removeChild(label);
-    const headerRow = document.querySelector("tr");
-
-    const nameHeader = document.createElement("th");
-    nameHeader.textContent = "Name";
-    headerRow.appendChild(nameHeader);
-
-    const authorHeader = document.createElement("th");
-    authorHeader.textContent = "Author";
-    headerRow.appendChild(authorHeader);
-
-    const statusHeader = document.createElement("th");
-    statusHeader.textContent = "Status";
-    headerRow.appendChild(statusHeader);
-
-    const deleteSection = document.createElement("th");
-    headerRow.appendChild(deleteSection);
-  }
-
-  const bookRow = document.createElement("tr");
-  bookList.appendChild(bookRow);
-
-  const bookTitle = document.createElement("td");
-  bookTitle.textContent = name;
-  bookRow.appendChild(bookTitle);
-
-  const bookAuthor = document.createElement("td");
-  bookAuthor.textContent = author;
-  bookRow.appendChild(bookAuthor);
-
-  const bookStatus = document.createElement("td");
-  bookStatus.textContent = status;
-  bookRow.appendChild(bookStatus);
+  addBookToLibrary(myLibrary);
+  noBookWarning();
 }
 
-if (myLibrary.length === 0) {
-  label.textContent =
-    "You haven't added any books yet. Add books by entering the details above.";
-  bookList.appendChild(label);
-  label.classList.add("no-books");
+function addBookToLibrary(myLibrary) {
+  myLibrary.forEach((book, index) => {
+    const bookRow = document.createElement("tr");
+    bookList.appendChild(bookRow);
+
+    const titleCell = document.createElement("td");
+    titleCell.textContent = book.title;
+    bookRow.appendChild(titleCell);
+
+    // Add the book author to the row
+    const authorCell = document.createElement("td");
+    authorCell.textContent = book.author;
+    bookRow.appendChild(authorCell);
+
+    // Add the book read status to the row
+    const readCell = document.createElement("td");
+    readCell.textContent = book.read ? "Yes" : "No";
+    bookRow.appendChild(readCell);
+
+    // Add a delete button to the row
+    const deleteCell = document.createElement("td");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+      // Remove the book from the array
+      myLibrary.splice(index, 1);
+      // Remove the row from the table
+      bookRow.remove();
+      noBookWarning();
+    });
+    deleteCell.appendChild(deleteButton);
+    bookRow.appendChild(deleteCell);
+
+    // Add the new row to the book list
+    bookList.appendChild(bookRow);
+  });
 }
